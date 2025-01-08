dfcaa7205182597596eaad0ed63b451904e45a18
Nikol_Niven <ntopalusic@gmail.com>
2025-01-08 14:03:33 +0100

    first assignment of the day complete

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
+const tongueTwister = s1 + s2 + s3 + s4 + s5;
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,11 +20,12 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
+const result = `${part1.slice(0, 3)}${part1[3].toUpperCase()}${part2.slice(
+  0,
+  part2.length - 1,
+)}${part2[part2.length - 1].toUpperCase()}`;
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,24 +33,21 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
-
-
+console.log(tipAmount);
 
 /*******************************************
-    Iteration 2.2 | Generate Random Number
+    Iteration 2.2 | Generate Random Number/
+
 *******************************************/
-
+const randomNum = Math.random();
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomIntNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
-
-
+console.log(randomIntNumber);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -61,16 +56,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // true
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); //false
 
 const expression7 = a && a;
